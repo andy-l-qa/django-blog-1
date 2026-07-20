3e48fe3316bd9f926d1592f3f7d4e4d24deaa210
Andy L <oleskainbox@gmail.com>
2026-07-20 17:06:35 -0400

    Added decorator for user auth and a login by email feature.

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = 'login'
+
+AUTHENTICATION_BACKENDS = ['users.backends.EmailBackend']
 
 TINYMCE_DEFAULT_CONFIG = {
     'custom_undo_redo_levels': 100,
