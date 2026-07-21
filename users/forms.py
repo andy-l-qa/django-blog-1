cf2b5139e00113975577bc2fb8af9638854dbc4b
Andy L <oleskainbox@gmail.com>
2026-07-21 15:42:57 -0400

    Added option to change and recover user password.

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,8 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, SetPasswordForm, PasswordResetForm
 from django.contrib.auth import get_user_model
 from django_recaptcha.fields import ReCaptchaField
 from django_recaptcha.widgets import ReCaptchaV2Checkbox
-
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(help_text='A valid email address, please.', required=True)
@@ -39,3 +38,14 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ['first_name', 'last_name', 'email', 'image', 'description']
+
+class SetPasswordForm(SetPasswordForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['new_password1', 'new_password2']
+
+class PasswordResetForm(PasswordResetForm):
+    def __init__(self, *args, **kwargs):
+        super(PasswordResetForm, self).__init__(*args, **kwargs)
+
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox())
--- users/urls.py
+++ users/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     # path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/<username>', views.profile, name='profile'),
     path('activate/<uidb64>/<token>', views.activate, name='activate'),
+    path("password_change", views.password_change, name="password_change"),
+    path("password_reset", views.password_reset_request, name="password_reset"),
+    path('reset/<uidb64>/<token>', views.passwordResetConfirm, name='password_reset_confirm'),
 ]
